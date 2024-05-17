4cda380606646c5ba795ecc0a600ac4db0c58a71
am-i-dead <foxyze@yandex.ru>
2024-05-17 16:04:17 +0500

    script installs veracrypt

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo $AIRLINE
-echo $ENV
-echo $CLOUD
+wget https://launchpad.net/veracrypt/trunk/1.26.7/+download/veracrypt-console-1.26.7-Ubuntu-23.04-amd64.deb
+sudo dpkg -i veracrypt-console-1.26.7-Ubuntu-23.04-amd64.deb
+veracrypt -t -h
