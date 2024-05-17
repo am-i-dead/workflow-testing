c9102f728118e54d9a103417275ed3c3c7d7c8bd
am-i-dead <foxyze@yandex.ru>
2024-05-17 16:08:07 +0500

    script installs veracrypt and dependencies

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt-get install libfuse2 -y
+sudo apt-get install libpcsclite1 -y
+sudo apt-get install pcscd -y
+
 wget https://launchpad.net/veracrypt/trunk/1.26.7/+download/veracrypt-console-1.26.7-Ubuntu-23.04-amd64.deb
 sudo dpkg -i veracrypt-console-1.26.7-Ubuntu-23.04-amd64.deb
 veracrypt -t -h
